c29aa3a955793b13f5f02185ce87e271726b77f7
SoufianeOuldAmar <soufiane_ouldamar@outlook.com>
2024-03-18 18:51:12 +0100

    Added AccountDataModel.cs, modified AcoountData.json with customer info, deleted .placedholder in DataModels folder, modified AccountDataAccess.cs

---
+++ Project/DataAccess/AccountDataAccess.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public static class AccountDataAccess
+{
+    static string path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\DataSources\AccountData.json"));
+
+    public static List<AccountDataModel> LoadAll()
+    {
+        string json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<List<AccountDataModel>>(json);
+    }
+}

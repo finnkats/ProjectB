1064cd2683fc353e681025f3fc2a34f0c3408b49
Max Bretherton <maxob2005@gmail.com>
2024-03-18 16:04:38 +0100

    Created the basic layout how App.cs could look and created a basic example menu, which will be seen when program is run (for now)

--- Project/Presentation/App.cs
+++ Project/Presentation/App.cs
@@ -1,5 +1,25 @@
 public static class App{
-    public static void Start(){
+    // Initialize objects that will be used in the app
+    public static Menu? CurrentMenu;
 
+    // Add new menu's here
+        // real menu placeholder
+    public static Menu ExampleMenu1 = new("Example Menu 1");
+
+    public static void Start(){
+        // First menu the program will use
+        CurrentMenu = ExampleMenu1;
+
+
+        // Fill up objects
+
+        //  real menu placeholder
+
+        //  Example Menu 1
+        ExampleMenu1.AddAllOption("Boilerplate Option 1", Example.Menu1AddOption3);
+        ExampleMenu1.AddAllOption("Boilerplate Option 2", Example.Menu1RemoveOption3);
+        ExampleMenu1.AddAllOption("Boilerplate Option 3", Example.DoNothing);
+        ExampleMenu1.AddCurrentOption("Boilerplate Option 1");
+        ExampleMenu1.AddCurrentOption("Boilerplate Option 2");
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿App.Start();
+
+// for now
+int i = 0;
+bool run = true;
+if (App.CurrentMenu == null) return;
+while (run){
+    Console.Write(App.CurrentMenu.MenuString());
+    // input will be 0 if the the input is not an integer
+    Int32.TryParse(Console.ReadLine(), out int input);
+    Action? function = App.CurrentMenu.GetFunction(input);
+    if (function != null) function();
+    Console.WriteLine();
+    if (i++ == 10) run = false;
+}

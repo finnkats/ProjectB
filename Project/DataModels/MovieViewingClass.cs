c77c95c78fc3719f69cb9ae627031a2a9af93263
Finn <finnkats@pm.me>
2024-03-25 19:19:10 +0100

    Removed old files

--- Project/DataModels/MovieViewingClass.cs
+++
@@ -1,7 +0,0 @@
-class MovieViewing {
-        public string? Location { get; set; }
-        public string? Time { get; set; }
-        public string? Date { get; set; }
-        public string? Zaal { get; set; }
-        public string? Naam { get; set; }
-    }
--- Project/DataModels/TicketInfo.cs
+++
@@ -1,8 +0,0 @@
-class TicketInfo
-{
-    public int TicketId { get; set; }
-    public string? Location { get; set; }
-    public string? Date { get; set; }
-    public string? Time { get; set; }
-    public string? Zaal { get; set; }
-}

3ed376e976678ab44aeb073466a5c6232750810b
BennyShinichi <145767884+BennyShinichi@users.noreply.github.com>
2024-04-02 14:49:35 +0200

    Splitted the two classes into different files.

--- Project/DataModels/Play.cs
+++ Project/DataModels/Play.cs
@@ -1,5 +1,3 @@
-using System.Text.Json.Serialization;
-
 public class Play
 {
     public string ID {  get; set; }
@@ -16,28 +14,5 @@ public class Play
         PlayName = playname;
         Genre = genre;
         Active = active;
-    }
-}
-
-public class PlayDetails
-{
-    [JsonPropertyName("Location")]
-    public string Location { get; set; }
-    [JsonPropertyName("Time")]
-    public string Time { get; set; }
-    [JsonPropertyName("Room")]
-    public string Room { get; set; }
-    [JsonPropertyName("Date")]
-    public string Date { get; set; }
-    [JsonPropertyName("PlayName")]
-    public string PlayName { get; set; }
-
-    public PlayDetails(string location, string time, string room, string date, string playname)
-    {
-        Location = location;
-        Time = time;
-        Room = room;
-        Date = date;
-        PlayName = playname;
     }
 }

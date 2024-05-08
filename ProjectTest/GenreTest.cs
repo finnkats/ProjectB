c89c5d89bda35824195e9c73b591316d0a239a21
Max Bretherton <maxob2005@gmail.com>
2024-05-08 16:20:38 +0200

    Added GenreTest.cs

--- ProjectTest/GenreTest.cs
+++ ProjectTest/GenreTest.cs
@@ -1,18 +1,27 @@
-// namespace ProjectTest.GenreTest;
+namespace ProjectTest.GenreTest;
 
-// [TestClass]
-// public class GenreTest
-// {
-//     [TestInitialize]
-//     public void Reset(){
-//         TestDataFiller.FillApp();
-//     }
+[TestClass]
+public class GenreTest
+{
+    [TestInitialize]
+    public void Reset(){
+        TestDataFiller.FillApp();
+    }
 
-//     [TestMethod]
-//     public void AddGenreTest()
-//     {
-//         Assert.IsFalse(App.genreLogic.AddGenre("Genre 1", 9));
-//         Assert.IsFalse(App.genreLogic.AddGenre("Genre 2", 6));
-//     }
+    [TestMethod]
+    public void AddGenreTest()
+    {
+        Assert.IsFalse(App.genreLogic.AddGenre("Genre 1", 0));
+        Assert.IsTrue(App.genreLogic.AddGenre("Genre 5", 0));
+    }
 
-// }
+    [TestMethod]
+    public void NewGenreTest()
+    {
+        int count = App.Genres.Count;
+        App.genreLogic.AddGenre("Genre 5", 0);
+        
+        Assert.AreEqual(1, App.Genres.Count - count);
+        Assert.AreEqual("Genre 5", App.Genres["ID4"].Name);
+    }
+}

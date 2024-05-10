76d06d0a0da1d864f8d18ba64c47007a098bd2b3
Finn <finnkats@pm.me>
2024-05-10 13:42:09 +0200

    make some changes to the menu and fixed some printing issues

--- Project/Logic/PlayLogic.cs
+++ Project/Logic/PlayLogic.cs
@@ -32,59 +32,6 @@ public static class PlayLogic
 
     }
 
-    // returns a string (which is basically a menu)
-    // and a Dictionary of int and string (where int is the index of the option shown in the menu) and
-    // string is the string of the date
-    public static (string?, Dictionary<int, string>?) GetDates(string selectedLocation, List<Play> playOptions){
-        if (playOptions.Count() == 0) return (null, null);
-        string? datesString = "";
-        datesString += "Available dates:\n";
-
-        HashSet<string> availableDates = new HashSet<string>();
-        foreach (var viewing in playOptions)
-        {
-            if (viewing.Location == selectedLocation)
-            {
-                availableDates.Add(viewing.Date);
-            }
-        }
-        List<string> availableDatesOrdered = availableDates.OrderBy(date => DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture)).ToList();
-
-        int dateCounter = 1;
-        Dictionary<int, string> dateOptions = new Dictionary<int, string>();
-        foreach (var date in availableDatesOrdered)
-        {
-            datesString += $"{dateCounter}: {date}\n";
-            dateOptions.Add(dateCounter, date);
-            dateCounter++;
-        }
-        
-        if (datesString == "Available dates:\n") datesString = null;
-        return (datesString, dateOptions);
-    }
-
-    // Does the same as GetDates but then with times
-    public static (string?, Dictionary<int, string>?) GetTimes(string selectedLocation, string chosenDate, List<Play> playOptions){
-        if (playOptions.Count() == 0) return (null, null);
-        string? timesString = $"Available times on {chosenDate}:\n";
-        int timeCounter = 1;
-        Dictionary<int, string> timeOptions = new Dictionary<int, string>();
-
-        var playOptionsOrdered = playOptions.OrderBy(performance => performance.Time).ToList();
-        foreach (var viewing in playOptionsOrdered)
-        {
-            if (viewing.Location == selectedLocation && viewing.Date == chosenDate)
-            {
-                timesString += $"{timeCounter}: {viewing.Time} in {App.Halls[viewing.Hall].Name}\n";
-                timeOptions.Add(timeCounter, viewing.Time);
-                timeCounter++;
-            }
-        }
-
-        if (timesString == $"Available times on {chosenDate}:\n") timesString = null;
-        return (timesString, timeOptions);
-    }
-
     public static bool AddPlay(string location, string time, string date, string hall, string playId){
         if (!App.Plays.ContainsKey(playId)) return false;
         Play newPlay = new(location, time, date, hall, playId);

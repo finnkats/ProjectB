f78d4130036b16c7e2d02de86abc5326d45c31a8
SoufianeOuldAmar <soufiane_ouldamar@outlook.com>
2024-05-20 14:00:35 +0200

    Changed array structure

--- Project/DataModels/Layout.cs
+++ Project/DataModels/Layout.cs
@@ -3,10 +3,10 @@ using System;
 public class Layout
 {
     // Property to hold the 2D array of seats
-    public int[,] Seats { get; set; }
+    public int[][] Seats { get; set; }
     public int SeatAmount { get; set; }
 
-    public Layout(int[,] seats, int seatAmount)
+    public Layout(int[][] seats, int seatAmount)
     {
         Seats = seats;
         if (seatAmount > 99)

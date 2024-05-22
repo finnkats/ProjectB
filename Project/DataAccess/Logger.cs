aaaf2aa7108463057f914183f1733b745df3f6c8
Finn <finnkats@pm.me>
2024-05-22 10:45:57 +0200

    final edit on logger class

--- Project/DataAccess/Logger.cs
+++ Project/DataAccess/Logger.cs
@@ -1,6 +1,18 @@
-public abstract class Logger {
-    protected abstract string _filePath {get; set;} // "DataModels/Logs/idk.csv"
-    protected abstract string _headers {get; set;}  // "item1,item2,item3,item4"
-    public abstract void LogAction();
+public abstract class Logger
+{
+    protected string FilePath { get; set; }  // Path to the log file
+    protected string Headers { get; set; }   // Headers for the CSV file
 
+    // Constructor for the abstract class
+    protected Logger(string filePath, string headers)
+    {
+        FilePath = filePath;
+        Headers = headers;
+    }
+
+    // Abstract method to log an action, to be implemented by derived classes
+    public abstract void LogAction(string action, object obj);
+
+    // Method to write a log entry to the CSV file
+    protected abstract void WriteToCsv(string logEntry);
 }

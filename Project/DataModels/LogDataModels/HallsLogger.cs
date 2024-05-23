443bacaae25e5a10abb6274c8bddf06cbcf19d97
Finn <finnkats@pm.me>
2024-05-23 23:29:35 +0200

    made nullable

--- Project/DataModels/LogDataModels/HallsLogger.cs
+++ Project/DataModels/LogDataModels/HallsLogger.cs
@@ -4,7 +4,7 @@ public class HallsLogger : Logger
     protected override string Headers {get; set;} = "Time, User, Action, Hall info";
     public HallsLogger(){ }
 
-    public override void LogAction(string action, object hallInfo)
+    public override void LogAction(string action, object? hallInfo)
     {
         string logEntry = $"{DateTime.Now}, {App.LoggedInUsername}, {action}, {hallInfo}";
         WriteToCsv(logEntry);
--- Project/DataModels/LogDataModels/LocationLogger.cs
+++ Project/DataModels/LogDataModels/LocationLogger.cs
@@ -4,7 +4,7 @@ public class LocationLogger : Logger
     protected override string Headers {get; set;} = "Time, User, Action, Location info";
     public LocationLogger(){ }
 
-    public override void LogAction(string action, object locationInfo)
+    public override void LogAction(string action, object? locationInfo)
     {
         string logEntry = $"{DateTime.Now}, {App.LoggedInUsername}, {action}, {locationInfo}";
         WriteToCsv(logEntry);

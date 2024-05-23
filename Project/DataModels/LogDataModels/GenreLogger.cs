75e03e3eae80848273e3247de9e0be3ac6c45080
Finn Kats <finnkats@pm.me>
2024-05-23 14:24:05 +0200

    changed obj name

--- Project/DataModels/LogDataModels/GenreLogger.cs
+++ Project/DataModels/LogDataModels/GenreLogger.cs
@@ -1,12 +1,12 @@
 public class GenreLogger : Logger
 {
     protected override string FilePath {get; set;} = "DataSources/LogFiles/Genres.csv";
-    protected override string Headers {get; set;} = "Time, User, Action, Object";
+    protected override string Headers {get; set;} = "Time, User, Action, Genre info";
     public GenreLogger(){ }
 
-    public override void LogAction(string action, object obj)
+    public override void LogAction(string action, object genreInfo)
     {
-        string logEntry = $"{DateTime.Now}, {App.LoggedInUsername}, {action}, {obj}";
+        string logEntry = $"{DateTime.Now}, {App.LoggedInUsername}, {action}, {genreInfo}";
         WriteToCsv(logEntry);
     }
 }

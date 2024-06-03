ee73ad0a007a3b0d0a93aaad8e00f56e52e6ccf8
Max Bretherton <maxob2005@gmail.com>
2024-06-03 13:46:00 +0200

    Changed it so it asks if the genre should stay, instead of be removed, and also shows what genres there currently are

--- Project/Presentation/PerformancePresentation.cs
+++ Project/Presentation/PerformancePresentation.cs
@@ -66,27 +66,26 @@ public class PerformancePresentation : PresentationBase<Performance>{
             if (choice == 0) return;
             if (choice == 2){
                 Console.Clear();
-                List<string> currentGenres = new();
-                foreach (var genreId in Logic.Dict[performanceId].Genres){
-                    currentGenres.Add(App.Genres[genreId].Name);
-                }
                 List<string> RemovedGenreIds = new();
+                string sep = ", ";
                 bool printBreadCrumb = true;
+                var GenresCopy = Logic.Dict[performanceId].Genres.ToList();
                 foreach (var genreId in Logic.Dict[performanceId].Genres){
                     if (printBreadCrumb)
                     {
                         Console.WriteLine($"Front Page -> Home Page -> Modify Performances -> {Logic.Dict[performanceId].Name} -> Change Genre\n");
                     }
                     printBreadCrumb = false;
-                    Console.Write($"Do you want to remove '{App.Genres[genreId].Name}' from '{Logic.Dict[performanceId].Name}'? (Y/N) \n\n> ");
-                    string removeGenre = Console.ReadLine()?.ToUpper() ?? "";
-                    if (removeGenre.StartsWith("Y")){
-                        currentGenres.Remove(App.Genres[genreId].Name);
-                        RemovedGenreIds.Add(genreId);
+                    Console.WriteLine($"Current Genres: {String.Join(sep, GenresCopy.Select(genre => App.Genres[genre].Name))}");
+                    Console.Write($"Do you want '{Logic.Dict[performanceId].Name}' to keep the Genre '{App.Genres[genreId].Name}'? (Y/N) \n> ");
+                    string keepGenre = Console.ReadLine()?.ToUpper() ?? "";
+                    if (!keepGenre.StartsWith("Y")){
+                        GenresCopy.Remove(genreId);
+                        Console.WriteLine($"Removed {App.Genres[genreId].Name}");
                     }
                     Console.WriteLine();
                 }
-                RemovedGenreIds.ForEach(genreId => Logic.Dict[performanceId].Genres.Remove(genreId));
+                Logic.Dict[performanceId].Genres = GenresCopy;
 
                 Console.Clear();
                 List<string> genres = App.genrePresentation.GetItemList(performanceId);
